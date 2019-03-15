eb09213b58cae7d7ee307d9ed17b6b24993fe571
bjut-bluecat <w@wtekiMacBook-Air.local>
2019-03-15 15:21:17 +0800

    Initial Commit

---
+++ 模拟银行排队/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  模拟银行排队
+//
+//  Created by 蓝猫 on 2019/3/15.
+//  Copyright © 2019年 蓝猫. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
